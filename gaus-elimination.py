3a4b63bb17ad7ea0b4b369555dc6e087d7722b81
Reeve Barreto <81756283+reeveboy@users.noreply.github.com>
2021-11-09 21:44:17 +0530

    Add files via upload

---
+++ gaus-elimination.py
@@ -0,0 +1,47 @@
+# Gausian Elimination
+
+import numpy as np
+import sys
+ 
+n = int(input('Enter number of unknowns: '))
+matrix = np.zeros((n,n+1))
+solutions = np.zeros(n)
+
+# Inputing Matrix
+print('Enter Augmented Matrix Coefficients:')
+for i in range(n):
+    for j in range(n+1):
+        matrix[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
+
+# Apply Gausian Elimination Algorithm 
+for i in range(n):
+    if matrix[i][i] == 0.0:
+        sys.exit('Divide by zero detected!')
+         
+    for j in range(i+1, n):
+        ratio = matrix[j][i]/matrix[i][i]
+         
+        for k in range(n+1):
+            matrix[j][k] = matrix[j][k] - ratio * matrix[i][k]
+ 
+# Solving and storing last unknown
+solutions[n-1] = matrix[n-1][n]/matrix[n-1][n-1]
+ 
+# Back Substituting and solving other unknowns
+for i in range(n-2,-1,-1):
+    solutions[i] = matrix[i][n]
+     
+    for j in range(i+1,n):
+        solutions[i] = solutions[i] - matrix[i][j]*solutions[j]
+     
+    solutions[i] = solutions[i]/matrix[i][i]
+ 
+# Printing solutions
+print('\nThe solution is: ')
+for i in range(n):
+    print(f'X{i+1} = {solutions[i]}', end = '\t')
+
+print()
+print()
+
+print(matrix)
